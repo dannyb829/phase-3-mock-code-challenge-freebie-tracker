08f8e0b9f627c1e0daeab17460bae16afa0f4252
Daniel <dannyb829@gmail.com>
2022-04-11 23:33:25 -0400

    commit tree c9661d0f615825f64314b8a0e0734b652b8078e0

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+c1 = Company.create(name: "Google", founding_year: 1998)
+c2 = Company.create(name: "Facebook", founding_year: 2004)
+c3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+c4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+d1 = Dev.create(name: "Rick")
+d2 = Dev.create(name: "Morty")
+d3 = Dev.create(name: "Mr. Meseeks")
+d4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(name:"some toy",dev:d2,company:c4)
+Freebie.create(name:"some gadget",dev:d4{ |variable|  ,company:c3)
+Freebie.create(name:"some gas",dev:d3,company:c2)
+Freebie.create(name:"some game",dev:d1,company:c1
+
 
 puts "Seeding done!"
